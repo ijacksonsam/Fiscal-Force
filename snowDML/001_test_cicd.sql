746e201288a6b399ca9dfbaa4e7ab203efe24eaf
unknown <Student3@IND-LABS.CAREERATG.COM>
2025-10-06 21:47:10 +0530

    testing basic cicd8

--- snowDML/001_test_cicd.sql
+++ snowDML/001_test_cicd.sql
@@ -1,12 +1,12 @@
 -- Insert sample data into TEST_TABLE
-INSERT INTO TEST_TABLE (ID, NAME) VALUES
+INSERT INTO TEST_TABLE1 (ID, NAME) VALUES
     (1, 'Alice'),
     (2, 'Bob'),
     (3, 'Charlie');
 
 -- Optional: another insert
-INSERT INTO TEST_TABLE (ID, NAME) VALUES
+INSERT INTO TEST_TABLE1 (ID, NAME) VALUES
     (4, 'David');
 -- Optional: another insert
-INSERT INTO TEST_TABLE (ID, NAME) VALUES
+INSERT INTO TEST_TABLE1 (ID, NAME) VALUES
     (5, 'Ankit');
